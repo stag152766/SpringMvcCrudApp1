82e3f8e423df55a76bc1f376f7f4908757e57902
stag152766 <45874547+stag152766@users.noreply.github.com>
2021-01-04 16:52:11 +0700

    add lesson22

--- src/main/java/ru/alishev/springcourse/config/controllers/PeopleController.java
+++ src/main/java/ru/alishev/springcourse/config/controllers/PeopleController.java
@@ -4,10 +4,9 @@ package ru.alishev.springcourse.config.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import ru.alishev.springcourse.config.dao.PersonDAO;
+import ru.alishev.springcourse.config.models.Person;
 
 @Controller
 @RequestMapping("/people")
@@ -34,6 +33,24 @@ public class PeopleController {
         //  Получим одного человека по id из DAO и на отображение в представление
         model.addAttribute("person", personDAO.show(id));
         return "people/show";
+    }
+
+//    @GetMapping("/new")
+//    // Реализуем форму для объета Person
+//    public String newPerson(Model model){
+//        model.addAttribute("person", new Person());
+//        return "people/new";
+//    }
+
+    @GetMapping("/new")
+    public String newPerson(@ModelAttribute("person") Person person){
+        return "people/new";
+    }
+
+    @PostMapping()
+    public String create(@ModelAttribute("person") Person person){
+        personDAO.save(person);
+        return "redirect:/people";
     }
 
 }
--- src/main/java/ru/alishev/springcourse/config/models/Person.java
+++ src/main/java/ru/alishev/springcourse/config/models/Person.java
@@ -5,10 +5,14 @@ public class Person {
     private int id;
     private String name;
 
+    public Person() {
+    }
+
     public Person(int id, String name) {
         this.id = id;
         this.name = name;
     }
+
 
     public int getId() {
         return id;
