3ffd2bb2874bf3107c557a16f9d7b440c3ea583a
stag152766 <45874547+stag152766@users.noreply.github.com>
2021-01-06 00:03:27 +0700

    add lesson 24

--- src/main/java/ru/alishev/springcourse/config/dao/PersonDAO.java
+++ src/main/java/ru/alishev/springcourse/config/dao/PersonDAO.java
@@ -14,10 +14,10 @@ public class PersonDAO {
 
     {
         people = new ArrayList<>();
-        people.add(new Person(++PEOPLE_COUNT, "Tom"));
-        people.add(new Person(++PEOPLE_COUNT, "Bob"));
-        people.add(new Person(++PEOPLE_COUNT, "Mike"));
-        people.add(new Person(++PEOPLE_COUNT, "Kate"));
+        people.add(new Person(++PEOPLE_COUNT, "Tom", 24, "tom@,mail.ru"));
+        people.add(new Person(++PEOPLE_COUNT, "Bob",52,"bob@mail.ru"));
+        people.add(new Person(++PEOPLE_COUNT, "Mike", 18,"mike@mail.ru"));
+        people.add(new Person(++PEOPLE_COUNT, "Kate",34,"kate@mail.ru"));
     }
 
     public List<Person> index(){
@@ -36,6 +36,9 @@ public class PersonDAO {
     public void update(int id, Person updatePerson) {
         Person personToBeUpdated = show(id);
         personToBeUpdated.setName(updatePerson.getName());
+        personToBeUpdated.setAge(updatePerson.getAge());
+        personToBeUpdated.setEmail(updatePerson.getEmail());
+
     }
 
     public void delete(int id) {
--- src/main/java/ru/alishev/springcourse/config/models/Person.java
+++ src/main/java/ru/alishev/springcourse/config/models/Person.java
@@ -1,16 +1,31 @@
 package ru.alishev.springcourse.config.models;
 
+import javax.validation.constraints.*;
+
 public class Person {
 
     private int id;
+
+
+    @NotEmpty(message = "Name should not be empty")
+    @Size(min=2, max=30)
     private String name;
+
+    @Min(value=1, message = "Age should be greater than 0")
+    private int age;
+
+    @NotEmpty(message = "Email should not be empty")
+    @Email(message = "Email should be valid")
+    private String email;
 
     public Person() {
     }
 
-    public Person(int id, String name) {
+    public Person(int id, String name, int age, String email) {
         this.id = id;
         this.name = name;
+        this.age = age;
+        this.email = email;
     }
 
 
@@ -30,5 +45,19 @@ public class Person {
         this.name = name;
     }
 
+    public int getAge() {
+        return age;
+    }
 
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
